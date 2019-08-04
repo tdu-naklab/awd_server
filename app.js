f7e3f3efcd3fc3d0767cb4c572315de250e6b685
sota <shu.cyclone@gmail.com>
2019-08-04 19:04:07 +0900

    WIP: socket.io

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const http = require('http');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
+const app = express();
+module.exports = app;
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const racesRouter = require('./routes/races');
 
-const app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/static', express.static('public'));
 app.use('/users', usersRouter);
 app.use('/races', racesRouter);
 
@@ -39,5 +43,3 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-module.exports = app;
---
+++ races.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS races;
+CREATE TABLE `races` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `course` int(1) NOT NULL,
+  `status` int(1) DEFAULT '0' NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS race_user;
+CREATE TABLE `race_user` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `race_id` int(4) NOT NULL,
+  `user_id` int(4) NOT NULL,
+  `raptime` int(8),
+  `lane` int(1) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
